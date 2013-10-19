251b6356caa03ef4f4720652f1505542bd558cb2
spyboost <spyboost@gmail.com>
2013-10-19 12:54:17 -0400

    alignment

--- src/main/java/org/atmosphere/samples/chat/angular/Message.java
+++ src/main/java/org/atmosphere/samples/chat/angular/Message.java
@@ -19,41 +19,41 @@ import java.util.Date;
 
 public final class Message{
 
-  private String message;
-  private String author;
-  private long time;
+    private String message;
+    private String author;
+    private long time;
 
-  public Message(){
-    this("", "");
-  }
+    public Message(){
+        this("", "");
+    }
 
-  public Message(String author, String message){
-    this.author = author;
-    this.message = message;
-    this.time = new Date().getTime();
-  }
+    public Message(String author, String message){
+        this.author = author;
+        this.message = message;
+        this.time = new Date().getTime();
+    }
 
-  public String getMessage(){
-    return message;
-  }
+    public String getMessage(){
+        return message;
+    }
 
-  public String getAuthor(){
-    return author;
-  }
+    public String getAuthor(){
+        return author;
+    }
 
-  public void setAuthor(String author){
-    this.author = author;
-  }
+    public void setAuthor(String author){
+        this.author = author;
+    }
 
-  public void setMessage(String message){
-    this.message = message;
-  }
+    public void setMessage(String message){
+        this.message = message;
+    }
 
-  public long getTime(){
-    return time;
-  }
+    public long getTime(){
+        return time;
+    }
 
-  public void setTime(long time){
-    this.time = time;
-  }
+    public void setTime(long time){
+        this.time = time;
+    }
 }
--- src/main/java/org/atmosphere/samples/chat/angular/MessageEncoderDecoder.java
+++ src/main/java/org/atmosphere/samples/chat/angular/MessageEncoderDecoder.java
@@ -7,23 +7,23 @@ import org.codehaus.jackson.map.ObjectMapper;
 import java.io.IOException;
 
 public final class MessageEncoderDecoder implements Encoder<Message, String>, Decoder<String, Message>{
-  private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
-  @Override
-  public Message decode(final String s){
-    try{
-      return mapper.readValue(s, Message.class);
-    }catch(IOException e){
-      throw new RuntimeException(e);
+    @Override
+    public Message decode(final String s){
+        try{
+            return mapper.readValue(s, Message.class);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  @Override
-  public String encode(final Message message){
-    try{
-      return mapper.writeValueAsString(message);
-    }catch(IOException e){
-      throw new RuntimeException(e);
+    @Override
+    public String encode(final Message message){
+        try{
+            return mapper.writeValueAsString(message);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
