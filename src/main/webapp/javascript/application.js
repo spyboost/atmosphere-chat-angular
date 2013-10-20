fae0dbb2dd790d91049bc431014b69bbfcb079db
spyboost <spyboost@gmail.com>
2013-10-20 16:04:20 -0400

    cleanup

--- src/main/webapp/javascript/application.js
+++ src/main/webapp/javascript/application.js
@@ -8,24 +8,24 @@ function ChatController($scope, atmosphereService){
 
   var request = {
     url: '/chat',
-    contentType : 'application/json',
-    logLevel : 'debug',
-    transport : 'websocket',
-    trackMessageLength : true,
-    reconnectInterval : 5000,
+    contentType: 'application/json',
+    logLevel: 'debug',
+    transport: 'websocket',
+    trackMessageLength: true,
+    reconnectInterval: 5000,
     enableXDR: true,
-    timeout : 60000
+    timeout: 60000
   };
 
   var socket;
 
-  request.onOpen = function(response) {
+  request.onOpen = function(response){
     $scope.model.transport = response.transport;
     $scope.model.connected = true;
     $scope.model.content = 'Atmosphere connected using ' + response.transport;
   };
 
-  request.onClientTimeout = function(response) {
+  request.onClientTimeout = function(response){
     $scope.model.content = 'Client closed the connection after a timeout. Reconnecting in ' + request.reconnectInterval;
     $scope.model.connected = false;
     socket.push(atmosphere.util.stringifyJSON({ author: author, message: 'is inactive and closed the connection. Will reconnect in ' + request.reconnectInterval }));
@@ -34,52 +34,48 @@ function ChatController($scope, atmosphereService){
     }, request.reconnectInterval);
   };
 
-  request.onReopen = function(response) {
+  request.onReopen = function(response){
     $scope.model.connected = true;
     $scope.model.content = 'Atmosphere re-connected using ' + response.transport;
   };
 
-  <!-- For demonstration of how you can customize the fallbackTransport using the onTransportFailure function -->
-  request.onTransportFailure = function(errorMsg, request) {
+  //For demonstration of how you can customize the fallbackTransport using the onTransportFailure function
+  request.onTransportFailure = function(errorMsg, request){
     atmosphere.util.info(errorMsg);
     request.fallbackTransport = 'long-polling';
     $scope.model.header = 'Atmosphere Chat. Default transport is WebSocket, fallback is ' + request.fallbackTransport;
   };
 
-  request.onMessage = function (response) {
-    var message = response.responseBody;
-    try {
-      var json = atmosphere.util.parseJSON(message);
-    } catch (e) {
-      console.error("Error parsing JSON: ", message);
+  request.onMessage = function(response){
+    var responseText = response.responseBody;
+    try{
+      var message = atmosphere.util.parseJSON(responseText);
+      if(!$scope.model.logged && $scope.model.name)
+        $scope.model.logged = true;
+      else{
+        var date = typeof(message.time) === 'string' ? parseInt(message.time) : message.time;
+        $scope.model.messages.push({author: message.author, date: new Date(date), text: message.message});
+      }
+    }catch(e){
+      console.error("Error parsing JSON: ", responseText);
       throw e;
+    }finally{
+      $scope.model.inputEnabled = true;
     }
-
-    if (!$scope.model.logged && $scope.model.name) {
-      $scope.model.logged = true;
-    } else {
-      var date = typeof(json.time) === 'string' ? parseInt(json.time) : json.time;
-      addMessage(json.author, json.message, new Date(date));
-    }
-    $scope.model.inputEnabled = true;
   };
 
-  function addMessage(author, message, datetime) {
-    $scope.model.messages.push({author: author, date: datetime, text: message});
-  }
-
-  request.onClose = function(response) {
+  request.onClose = function(response){
     $scope.model.connected = false;
     $scope.model.content = 'Server closed the connection after a timeout';
     socket.push(atmosphere.util.stringifyJSON({ author: $scope.model.name, message: 'disconnecting' }));
   };
 
-  request.onError = function(response) {
+  request.onError = function(response){
     $scope.model.content = "Sorry, but there's some problem with your socket or the server is down";
     $scope.model.logged = false;
   };
 
-  request.onReconnect = function(request, response) {
+  request.onReconnect = function(request, response){
     $scope.model.content = 'Connection lost. Trying to reconnect ' + request.reconnectInterval;
     $scope.model.connected = false;
   };
@@ -87,13 +83,13 @@ function ChatController($scope, atmosphereService){
   socket = atmosphereService.subscribe(request);
 
   var input = $('#input');
-  input.keydown(function(event) {
+  input.keydown(function(event){
     var me = this;
     var msg = $(me).val();
-    if(msg && msg.length > 0 && event.keyCode === 13) {
+    if(msg && msg.length > 0 && event.keyCode === 13){
       $scope.$apply(function(){
         // First message is always the author's name
-        if (!$scope.model.name)
+        if(!$scope.model.name)
           $scope.model.name = msg;
 
         socket.push(atmosphere.util.stringifyJSON({author: $scope.model.name, message: msg}));
